e010b4c98df0a26ee6b464097111348a5b51fb53
rkansal47 <raghavk1997@yahoo.co.in>
2022-01-27 22:08:44 -0800

    clopper pearson intervals

--- overlap_yield.py
+++ overlap_yield.py
@@ -1,7 +1,10 @@
 import awkward as ak
 import uproot
 import numpy as np
-import pandas as pd
+from coffea.hist import clopper_pearson_interval
+
+# import pandas as pd
+
 
 BDT_VAR = "disc_qcd_and_ttbar_Run2_enhanced_v8p2"
 
@@ -26,9 +29,7 @@ branches = [
     BDT_VAR,
 ]
 
-
 years = ["2016", "2017", "2018"]
-
 
 files = [
     "root://cmseos.fnal.gov//store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/20220124/option5/combined/2016/GluGluToHHTo4B_node_cHHH1_TuneCUETP8M1_PSWeights_13TeV-powheg-pythia8_tree_BDTs.root",
@@ -37,12 +38,16 @@ files = [
 ]
 
 events = uproot.concatenate(files, branches)
+# events_full = uproot.concatenate(files)
 
-events.fields
+nbtagged_jets = (
+    ak.values_astype((events.jet1Pt > 0), int)
+    + ak.values_astype((events.jet2Pt > 0), int)
+    + ak.values_astype((events.jet3Pt > 0), int)
+    + ak.values_astype((events.jet4Pt > 0), int)
+)
 
-
-events = uproot.concatenate(files, branches)
-
+resolved_accept = nbtagged_jets >= 3
 
 pt_cuts = np.arange(300, 601, 50)
 
@@ -51,8 +56,11 @@ NUM_REGIONS = 4
 # Regions 1-3 = Bins 1-3
 # Region 4 = QCD CR
 
-overlaps_boosted = []
-overlaps_resolved = []
+numerators = []
+denominators_boosted = []
+denominators_resolved = []
+
+tag = "3b_bdt"
 
 for ptcut in pt_cuts:
     fail_resolved_veto_pt = (
@@ -63,15 +71,6 @@ for ptcut in pt_cuts:
         | (events.fatJet1NSubJets < 2)
         | (events.fatJet2NSubJets < 2)
     )
-
-    nbtagged_jets = (
-        ak.values_astype((events.jet1Pt > 0), int)
-        + ak.values_astype((events.jet2Pt > 0), int)
-        + ak.values_astype((events.jet3Pt > 0), int)
-        + ak.values_astype((events.jet4Pt > 0), int)
-    )
-
-    resolved_accept = nbtagged_jets >= 3
 
     # should technically include BDT selection for this?
     boosted_acceptances = []
@@ -112,27 +111,75 @@ for ptcut in pt_cuts:
         (events[BDT_VAR] > 0.03) & (events.fatJet2PNetXbb < 0.95) & baseline_cut
     )
 
-    overlaps_boosted.append(
+    numerators.append(
         [
             ak.sum(fail_resolved_veto_pt & resolved_accept & boosted_acceptances[i])
-            / ak.sum(boosted_acceptances[i])
             for i in range(NUM_REGIONS)
         ]
     )
 
-    overlaps_resolved.append(
-        [
-            ak.sum(fail_resolved_veto_pt & resolved_accept & boosted_acceptances[i])
-            / ak.sum(fail_resolved_veto_pt & resolved_accept)
-            for i in range(NUM_REGIONS)
-        ]
+    denominators_boosted.append([ak.sum(boosted_acceptances[i]) for i in range(NUM_REGIONS)])
+    denominators_resolved.append(
+        [ak.sum(fail_resolved_veto_pt & resolved_accept) for i in range(NUM_REGIONS)]
     )
 
 
-table = np.concatenate((pt_cuts[:, np.newaxis], overlaps_boosted), axis=1)
-pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(NUM_REGIONS)])
-pddf.to_csv("overlaps_boosted_3b_bdt.csv", index=False)
+numerators = np.array(numerators)
+denominators_boosted = np.array(denominators_boosted)
+denominators_resolved = np.array(denominators_resolved)
 
-table = np.concatenate((pt_cuts[:, np.newaxis], overlaps_resolved), axis=1)
-pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(NUM_REGIONS)])
-pddf.to_csv("overlaps_resolved_3b_bdt.csv", index=False)
+overlaps_boosted = numerators / denominators_boosted
+overlaps_resolved = numerators / denominators_resolved
+
+cp_boosted = clopper_pearson_interval(numerators, denominators_boosted)
+errors_boosted = (cp_boosted[1] - cp_boosted[0]) / 2
+cp_resolved = clopper_pearson_interval(numerators, denominators_resolved)
+errors_resolved = (cp_resolved[1] - cp_resolved[0]) / 2
+
+######################################
+# Table Formatting
+######################################
+
+decimals_boosted = -(np.floor(np.log10(errors_boosted))).astype(int)
+decimals_boosted[errors_boosted == 0] = 0
+decimals_boosted -= ((errors_boosted * 10 ** decimals_boosted) >= 9.5).astype(int)
+decimals_boosted = np.clip(decimals_boosted, a_min=0, a_max=3)
+
+output_list_boosted = []
+for i in range(overlaps_boosted.shape[0]):
+    row = []
+    for j in range(overlaps_boosted.shape[1]):
+        row.append(
+            f"{overlaps_boosted[i][j]:.{decimals_boosted[i][j]}f}±{errors_boosted[i][j]:.{decimals_boosted[i][j]}f}"
+            if errors_boosted[i][j] >= 0.0005
+            else "0"
+        )
+    output_list_boosted.append(row)
+
+np.savetxt(f"overlaps_boosted_{tag}.csv", np.array(output_list_boosted), fmt="%s", delimiter=",")
+
+decimals_resolved = -(np.floor(np.log10(errors_resolved))).astype(int)
+decimals_resolved[errors_resolved == 0] = 0
+decimals_resolved -= ((errors_resolved * 10 ** decimals_resolved) >= 9.5).astype(int)
+decimals_resolved = np.clip(decimals_resolved, a_min=0, a_max=3)
+
+output_list_resolved = []
+for i in range(overlaps_resolved.shape[0]):
+    row = []
+    for j in range(overlaps_resolved.shape[1]):
+        row.append(
+            f"{overlaps_resolved[i][j]:.{decimals_resolved[i][j]}f}±{errors_resolved[i][j]:.{decimals_resolved[i][j]}f}"
+            if errors_resolved[i][j] >= 0.0005
+            else "0"
+        )
+    output_list_resolved.append(row)
+
+np.savetxt(f"overlaps_resolved_{tag}.csv", np.array(output_list_resolved), fmt="%s", delimiter=",")
+
+# table = np.concatenate((pt_cuts[:, np.newaxis], overlaps_boosted), axis=1)
+# pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(NUM_REGIONS)])
+# pddf.to_csv(f"overlaps_boosted_{tag}.csv", index=False)
+#
+# table = np.concatenate((pt_cuts[:, np.newaxis], overlaps_resolved), axis=1)
+# pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(NUM_REGIONS)])
+# pddf.to_csv(f"overlaps_resolved_{tag}.csv", index=False)
