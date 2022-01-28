4ae1fe64ba174c0570ca4c1fa479fdc340acb330
rkansal47 <raghavk1997@yahoo.co.in>
2022-01-27 18:09:11 -0800

    bdt cuts

--- overlap_yield.py
+++ overlap_yield.py
@@ -3,6 +3,8 @@ import uproot
 import numpy as np
 import pandas as pd
 
+BDT_VAR = "disc_qcd_and_ttbar_Run2_enhanced_v8p2"
+
 branches = [
     "fatJet1Pt",
     "fatJet1MassSD",
@@ -21,46 +23,39 @@ branches = [
     "xsecWeight",
     "fatJet1NSubJets",
     "fatJet2NSubJets",
+    BDT_VAR,
 ]
+
 
 years = ["2016", "2017", "2018"]
 
+
 files = [
-    "root://cmseos.fnal.gov//store/user/cmantill/analyzer/v4_Jan24_ak8_option8_2016/signal/parts/GluGluToHHTo4B_node_cHHH1_TuneCUETP8M1_PSWeights_13TeV-powheg-pythia8_tree.root:Events"
+    "root://cmseos.fnal.gov//store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/20220124/option5/combined/2016/GluGluToHHTo4B_node_cHHH1_TuneCUETP8M1_PSWeights_13TeV-powheg-pythia8_tree_BDTs.root",
+    "root://cmseos.fnal.gov//store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/20220124/option5/combined/2017/GluGluToHHTo4B_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8_tree_BDTs.root",
+    "root://cmseos.fnal.gov//store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/20220124/option5/combined/2018/GluGluToHHTo4B_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8_tree_BDTs.root",
 ]
-
-files += [
-    f"root://cmseos.fnal.gov//store/user/cmantill/analyzer/v4_Jan24_ak8_option8_{year}/signal/parts/GluGluToHHTo4B_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8_tree.root:Events"
-    for year in years
-    if year != "2016"
-]
-
-print(
-    "Fraction of overlap with (resolved_veto(pt) && boosted_accept(pt) && resolved_accept)/(boosted_accept(pt))"
-)
-
-print(
-    "(1): boosted_accept = fatJet1Pt>300 && fatJet2Pt>300 && fatJet1MassSD>50 && fatJet2MassRegressed>50 && ev.fatJet1PNetXbb>0.8 && ev.fatJet2PNetXbb>0.95"
-)
-
-print(
-    "(2): boosted_accept = fatJet1Pt>300 && fatJet2Pt>300 && fatJet1MassSD>50 && fatJet2MassRegressed>50 && ev.fatJet1PNetXbb>0.8 && ev.fatJet2PNetXbb>0.98"
-)
-
-print(
-    "(3): QCD_CR = fatJet1Pt>300 && fatJet2Pt>300 && fatJet1MassSD>50 && fatJet2MassRegressed>50 && ev.fatJet1PNetXbb>0.8 && ev.fatJet2PNetXbb<0.95"
-)
 
 events = uproot.concatenate(files, branches)
+
+events.fields
+
+
+events = uproot.concatenate(files, branches)
+
+
+pt_cuts = np.arange(300, 601, 50)
+
+NUM_REGIONS = 4
+
+# Regions 1-3 = Bins 1-3
+# Region 4 = QCD CR
 
 overlaps_boosted = []
 overlaps_resolved = []
 
-pt_cuts = np.arange(300, 601, 50)
-
-
 for ptcut in pt_cuts:
-    resolved_veto_pt = (
+    fail_resolved_veto_pt = (
         (events.fatJet1Pt < ptcut)
         | (events.fatJet2Pt < ptcut)
         | (events.fatJet1MassSD_noJMS < 30)
@@ -81,56 +76,49 @@ for ptcut in pt_cuts:
     # should technically include BDT selection for this?
     boosted_acceptances = []
 
-    # boosted accept 1
+    # boosted category 1
+    boosted_acceptances.append((events[BDT_VAR] > 0.43) & (events.fatJet2PNetXbb > 0.98))
+
+    # boosted category 2
     boosted_acceptances.append(
-        (events.fatJet1Pt > ptcut)
-        & (events.fatJet2Pt > ptcut)
-        & (events.fatJet1MassSD > 50)
-        & (events.fatJet2MassRegressed > 50)
-        & (events.fatJet1PNetXbb > 0.8)
-        & (events.fatJet2PNetXbb > 0.95)
+        (
+            ((events[BDT_VAR] < 0.43) & (events[BDT_VAR] > 0.11) & (events.fatJet2PNetXbb > 0.98))
+            | ((events[BDT_VAR] > 0.43) & (events.fatJet2PNetXbb > 0.95))
+        )
+        & ~boosted_acceptances[0]
     )
 
-    # boosted accept 2
+    # boosted category 3
     boosted_acceptances.append(
-        (events.fatJet1Pt > ptcut)
-        & (events.fatJet2Pt > ptcut)
-        & (events.fatJet1MassSD > 50)
-        & (events.fatJet2MassRegressed > 50)
-        & (events.fatJet1PNetXbb > 0.8)
-        & (events.fatJet2PNetXbb > 0.98)
+        ((events[BDT_VAR] > 0.03) & (events.fatJet2PNetXbb > 0.95))
+        & ~boosted_acceptances[0]
+        & ~boosted_acceptances[1]
     )
 
     # qcd cr
-    boosted_acceptances.append(
-        (events.fatJet1Pt > ptcut)
-        & (events.fatJet2Pt > ptcut)
-        & (events.fatJet1MassSD > 50)
-        & (events.fatJet2MassRegressed > 50)
-        & (events.fatJet1PNetXbb > 0.8)
-        & (events.fatJet2PNetXbb < 0.95)
-    )
+    boosted_acceptances.append((events[BDT_VAR] > 0.03) & (events.fatJet2PNetXbb < 0.95))
 
     overlaps_boosted.append(
         [
-            ak.sum(resolved_veto_pt & resolved_accept & boosted_acceptances[i])
+            ak.sum(fail_resolved_veto_pt & resolved_accept & boosted_acceptances[i])
             / ak.sum(boosted_acceptances[i])
-            for i in range(3)
+            for i in range(NUM_REGIONS)
         ]
     )
 
     overlaps_resolved.append(
         [
-            ak.sum(resolved_veto_pt & resolved_accept & boosted_acceptances[i])
-            / ak.sum(resolved_veto_pt & resolved_accept)
-            for i in range(3)
+            ak.sum(fail_resolved_veto_pt & resolved_accept & boosted_acceptances[i])
+            / ak.sum(fail_resolved_veto_pt & resolved_accept)
+            for i in range(NUM_REGIONS)
         ]
     )
 
+
 table = np.concatenate((pt_cuts[:, np.newaxis], overlaps_boosted), axis=1)
-pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(3)])
+pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(NUM_REGIONS)])
 pddf.to_csv("overlaps_boosted_3b.csv", index=False)
 
 table = np.concatenate((pt_cuts[:, np.newaxis], overlaps_resolved), axis=1)
-pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(3)])
+pddf = pd.DataFrame(table, columns=["pT cut"] + [f"Region {i + 1}" for i in range(NUM_REGIONS)])
 pddf.to_csv("overlaps_resolved_3b.csv", index=False)
